c5ac076bb4a620841f274358d8b70d3a6c973cb6
Tiago Maluta <tiago.maluta@gmail.com>
2013-06-05 12:01:17 -0300

    Second commit, backup it :)
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- src/br/edu/unifei/secomp2013/MainActivity.java
+++ src/br/edu/unifei/secomp2013/MainActivity.java
@@ -10,6 +10,7 @@ import org.apache.thrift.transport.TTransport;
 import tutorial.Calculator;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -17,26 +18,82 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private static final String TAG = null;
+	TTransport transport;
+	TProtocol protocol;
+	Calculator.Client client;
+	String ip;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Button b1 = (Button) findViewById(R.id.button1);
-		Button b2 = (Button) findViewById(R.id.button2);
-		Button b3 = (Button) findViewById(R.id.button3);
-		Button b4 = (Button) findViewById(R.id.button4);
-		Button b5 = (Button) findViewById(R.id.button5);
-		Button b6 = (Button) findViewById(R.id.button6);
-		Button b7 = (Button) findViewById(R.id.button7);
+		final Button b1 = (Button) findViewById(R.id.button1);
+		final Button b2 = (Button) findViewById(R.id.button2);
+		final Button b3 = (Button) findViewById(R.id.button3);
+		final Button b4 = (Button) findViewById(R.id.button4);
+		final Button b5 = (Button) findViewById(R.id.button5);
+		final Button b6 = (Button) findViewById(R.id.button6);
+		final Button b7 = (Button) findViewById(R.id.button7);
+		final Button button_connect = (Button) findViewById(R.id.btn_connect);
+
+		b1.setVisibility(View.INVISIBLE);
+		b2.setVisibility(View.INVISIBLE);
+		b3.setVisibility(View.INVISIBLE);
+		b4.setVisibility(View.INVISIBLE);
+		b5.setVisibility(View.INVISIBLE);
+		b6.setVisibility(View.INVISIBLE);
+		b7.setVisibility(View.INVISIBLE);
+
+		final TextView t1 = (TextView) findViewById(R.id.server_ip);
+
+		try {
+			Bundle bundle = getIntent().getExtras();
+			ip = bundle.getString("ip_address");
+			t1.setText(ip);
+		} catch (NullPointerException ex) {
+			ip="192.168.1.7";
+		}
+		t1.setText(ip);
 
 		//JavaClient jc = new JavaClient();
+		button_connect.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				boolean on = false;
+				Log.e(TAG,"connect");
+				//Toast.makeText(getApplicationContext(), "Connectando...", Toast.LENGTH_LONG).show();
+
+				try {
+					transport = new TSocket(ip, 9090);
+					transport.open();
+
+					protocol = new  TBinaryProtocol(transport);
+					client = new Calculator.Client(protocol);
+					on = true;
+				} catch (TException x) {
+					Log.e(TAG,"error on connecting");
+				} 
+
+				if (on) {
+					b1.setVisibility(View.VISIBLE);
+					b2.setVisibility(View.VISIBLE);
+					b3.setVisibility(View.VISIBLE);
+					b4.setVisibility(View.VISIBLE);
+					b5.setVisibility(View.VISIBLE);
+					b6.setVisibility(View.VISIBLE);
+					b7.setVisibility(View.VISIBLE);
+				}
+				//SetColors(1);
+			}
+		});
 
 		b1.setOnClickListener(new View.OnClickListener() {
 
@@ -44,7 +101,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Log.e(TAG,""+v.getId());
-				SetColors(1);
+				//client.add(1, arg1)
 			}
 		});
 
@@ -54,8 +111,6 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Log.e(TAG,""+v.getId());
-
-
 			}
 		});
 
@@ -69,7 +124,6 @@ public class MainActivity extends Activity {
 		});
 
 		b4.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -103,37 +157,26 @@ public class MainActivity extends Activity {
 				Log.e(TAG,""+v.getId());
 			}
 		});
-
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
-
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Log.e(TAG,""+item.getItemId());
-
-		showMsg("Ok");
+		Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+		startActivity(intent);
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void showMsg(String msg) {
-		EditText e = new EditText(getApplicationContext());
-		//	Toast toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
-		Toast toast = Toast.makeText(this, e.getText().toString(), Toast.LENGTH_LONG);
-		toast.setGravity(Gravity.CENTER, toast.getXOffset() / 2, toast.getYOffset() / 2);
-		toast.show();
-	}
-
-
 	public void SetColors(int id) {
 
-		try {
+		/*try {
 			TTransport transport;
 			transport = new TSocket("192.168.1.7", 9090);
 			transport.open();
@@ -154,6 +197,6 @@ public class MainActivity extends Activity {
 			transport.close();
 		} catch (TException x) {
 			x.printStackTrace();
-		} 
+		} */
 	}
 }
